86f28e90b49274e3badffa59e979777baa0efd02
Robert Wang <rwang5688@outlook.com>
2020-10-18 01:33:03 -0700

    extract get_blob function

--- create_visitors/cloud_storage_util.py
+++ create_visitors/cloud_storage_util.py
@@ -14,28 +14,37 @@ def get_storage_client():
     return storage_client
 
 
+def get_blob(bucket_name, blob_name):
+    storage_client = get_storage_client()
+    if storage_client is None:
+        print(f'get_blob: Failed to get storage client.')
+        return None
+
+    bucket = storage_client.bucket(bucket_name)
+    if bucket is None:
+        print(f'get_blob: Failed to get bucket {bucket_name}')
+        return None
+
+    blob = bucket.blob(blob_name)
+    if blob is None:
+        print(f'get_blob: Failed to get destination blob {blob_name}')
+        return None
+
+    return blob
+
+
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
 
-    storage_client = get_storage_client()
-    if storage_client is None:
-        print(f'upload_blob: Failed to get storage client.')
-        return False
-
-    bucket = storage_client.bucket(bucket_name)
-    if bucket is None:
-        print(f'upload_blob: Failed to get bucket {bucket_name}')
-        return False
-
-    blob = bucket.blob(destination_blob_name)
-    if blob is None:
+    destination_blob = get_blob(bucket_name, destination_blob_name)
+    if destination_blob is None:
         print(f'upload_blob: Failed to get destination blob {destination_blob_name}')
         return False
 
-    blob.upload_from_filename(source_file_name)
+    destination_blob.upload_from_filename(source_file_name)
 
     print(
         "upload_blob: Uploaded file {} to {} as {}".format(
@@ -52,22 +61,12 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     # source_blob_name = "storage-object-name"
     # destination_file_name = "local/path/to/file"
 
-    storage_client = get_storage_client()
-    if storage_client is None:
-        print(f'download_blob: Failed to get storage client.')
-        return False
-
-    bucket = storage_client.bucket(bucket_name)
-    if bucket is None:
-        print(f'download_blob: Failed to get bucket {bucket_name}')
-        return False
-
-    blob = bucket.blob(source_blob_name)
-    if blob is None:
+    source_blob = get_blob(bucket_name, source_blob_name)
+    if source_blob is None:
         print(f'download_blob: Failed to get source blob {source_blob_name}')
         return False
 
-    blob.download_to_filename(destination_file_name)
+    source_blob.download_to_filename(destination_file_name)
 
     print(
         "download_blob: Downloaded blob {} from {} as {}".format(
